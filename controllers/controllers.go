c4d37375d5c2bd171ef47774c9573d204358f877
Muhammad Daffa <36522826+daffainfo@users.noreply.github.com>
2021-07-18 20:34:05 +0700

    Shortening code

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,10 +1,34 @@
 package controllers
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 )
+
+//normal curl
+func normal_curl(url string) (string, int) {
+	req, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer req.Body.Close()
+	body, err := ioutil.ReadAll(req.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("\nStatus Code:\n" + strconv.Itoa(req.StatusCode))
+	fmt.Println("\nResponse Body:\n" + string(body))
+
+	defer req.Body.Close()
+
+	return string(body), req.StatusCode
+}
 
 func Asana(asana_access string) string {
 	req, err := http.NewRequest("GET", "https://app.asana.com/api/1.0/users/me", nil)
@@ -26,50 +50,40 @@ func Asana(asana_access string) string {
 	}
 }
 func Bing(bingapi string) string {
-	resp, err := http.Get("https://dev.virtualearth.net/REST/v1/Locations?CountryRegion=US&adminDistrict=WA&locality=Somewhere&postalCode=98001&addressLine=100%20Main%20St.&key=" + bingapi)
-	if err != nil {
-		log.Fatal(err)
+	url := "https://dev.virtualearth.net/REST/v1/Locations?CountryRegion=US&adminDistrict=WA&locality=Somewhere&postalCode=98001&addressLine=100%20Main%20St.&key=" + bingapi
+	a, b := normal_curl(url)
+	_ = a
+
+	if b == 200 {
+		return "\nBing Maps API Key valid"
+	} else {
+		return "\nBing Maps API Key invalid"
 	}
 
-	if resp.Status == "401 Unauthorized" {
-		return "Invalid API key"
-	} else {
-		return "API key valid"
-	}
 }
 
 func Bitly(bitly_token string) string {
-	error := "{\"status_code\":500,\"status_txt\":\"INVALID_ARG_ACCESS_TOKEN\",\"data\":[]}"
+	error_msg := "{\"status_code\":500,\"status_txt\":\"INVALID_ARG_ACCESS_TOKEN\",\"data\":[]}"
+	url := "https://api-ssl.bitly.com/v3/shorten?access_token=" + bitly_token + "&longUrl=https://www.google.com"
+	a, b := normal_curl(url)
 
-	req, err := http.Get("https://api-ssl.bitly.com/v3/shorten?access_token=" + bitly_token + "&longUrl=https://www.google.com")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer req.Body.Close()
-	body, err := ioutil.ReadAll(req.Body)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if string(body) == error {
-		return "Invalid API key"
+	if a != error_msg && b == 200 {
+		return "\nBit.ly Access token valid"
 	} else {
-		return "API key valid"
+		return "\nBit.ly Access token invalid"
 	}
 }
 
 func Branch(branch_key string, branch_secret string) string {
-	resp, err := http.Get("https://api2.branch.io/v1/app/" + branch_key + "?branch_secret=" + branch_secret)
-	if err != nil {
-		log.Fatal(err)
-	}
+	url := "https://api2.branch.io/v1/app/" + branch_key + "?branch_secret=" + branch_secret
 
-	if resp.Status == "400 Bad Request" {
-		return "Invalid API key"
+	a, b := normal_curl(url)
+	_ = a
+
+	if b == 200 {
+		return "\nBranch.IO Key and Secret valid"
 	} else {
-		return "API key valid"
+		return "\nBranch.IO Key and Secret invalid"
 	}
 }
 
@@ -94,63 +108,55 @@ func Buildkite(buildkite_access string) string {
 }
 
 func ButterCMS(branch_key string) string {
-	resp, err := http.Get("https://api.buttercms.com/v2/posts/?auth_token=" + branch_key)
-	if err != nil {
-		log.Fatal(err)
-	}
+	url := "https://api.buttercms.com/v2/posts/?auth_token=" + branch_key
 
-	if resp.Status == "401 Unauthorized" {
-		return "Invalid API key"
+	a, b := normal_curl(url)
+	_ = a
+
+	if b == 200 {
+		return "\nBranch.IO Key and Secret valid"
 	} else {
-		return "API key valid"
+		return "\nBranch.IO Key and Secret invalid"
 	}
 }
 
 func Circleci(circleci_token string) string {
-	resp, err := http.Get("https://circleci.com/api/v1.1/me?circle-token=" + circleci_token)
-	if err != nil {
-		log.Fatal(err)
-	}
+	url := "https://circleci.com/api/v1.1/me?circle-token=" + circleci_token
 
-	if resp.Status == "401 Unauthorized" {
-		return "Invalid API key"
+	a, b := normal_curl(url)
+	_ = a
+
+	if b == 200 {
+		return "\nCircleCI Access Token valid"
 	} else {
-		return "API key valid"
+		return "\nCircleCI Access Token invalid"
 	}
 }
 
 func Datadog(dog_api string, dog_application string) string {
-	resp, err := http.Get("https://api.datadoghq.com/api/v1/dashboard?api_key=" + dog_api + "&application_key=" + dog_application)
-	if err != nil {
-		log.Fatal(err)
-	}
+	url := "https://api.datadoghq.com/api/v1/dashboard?api_key=" + dog_api + "&application_key=" + dog_application
 
-	if resp.Status == "403 Forbidden" {
-		return "Invalid API key"
+	a, b := normal_curl(url)
+	_ = a
+
+	if b == 200 {
+		return "\nDataDog API key valid"
 	} else {
-		return "API key valid"
+		return "\nDataDog API key invalid"
 	}
 }
 
 func Loqate(loqate_key string) string {
-	error := "{\"Items\":[{\"Error\":\"2\",\"Description\":\"Unknown key\",\"Cause\":\"The key you are using to access the service was not found.\",\"Resolution\":\"Please check that the key is correct. It should be in the form AA11-AA11-AA11-AA11.\"}]}"
+	error_msg := "{\"Items\":[{\"Error\":\"2\",\"Description\":\"Unknown key\",\"Cause\":\"The key you are using to access the service was not found.\",\"Resolution\":\"Please check that the key is correct. It should be in the form AA11-AA11-AA11-AA11.\"}]}"
+	url := "http://api.addressy.com/Capture/Interactive/Find/v1.00/json3.ws?Key=" + loqate_key + "&Countries=US,CA&Language=en&Limit=5&Text=BHAR"
 
-	req, err := http.Get("http://api.addressy.com/Capture/Interactive/Find/v1.00/json3.ws?Key=" + loqate_key + "&Countries=US,CA&Language=en&Limit=5&Text=BHAR")
-	if err != nil {
-		log.Fatal(err)
-	}
+	a, b := normal_curl(url)
+	_ = b
 
-	defer req.Body.Close()
-	body, err := ioutil.ReadAll(req.Body)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if string(body) == error {
-		return "Invalid API key"
+	if a != error_msg {
+		return "\nDataDog API key valid"
 	} else {
-		return "API key valid"
+		return "\nDataDog API key invalid"
 	}
 }
 
@@ -195,14 +201,13 @@ func Travis(travis_token string) string {
 }
 
 func Wakatime(wakatime_key string) string {
-	resp, err := http.Get("https://wakatime.com/api/v1/users/current/projects/?api_key=" + wakatime_key)
-	if err != nil {
-		log.Fatal(err)
-	}
+	url := "https://wakatime.com/api/v1/users/current/projects/?api_key=" + wakatime_key
+	a, b := normal_curl(url)
+	_ = a
 
-	if resp.Status == "401 Unauthorized" {
-		return "Invalid API key"
+	if b == 200 {
+		return "\nWakaTime API Key valid"
 	} else {
-		return "API key valid"
+		return "\nWakaTime API Key invalid"
 	}
 }
--- main.go
+++ main.go
@@ -33,73 +33,73 @@ func main() {
 	fmt.Println("12. WakaTime API Key")
 
 	fmt.Println("\nChoose checkers:")
-	fmt.Scanln(&choose)
+	fmt.Scan(&choose)
 
 	if choose == 1 {
 		var asana_access string
-		fmt.Println("\nInput your Access Token")
-		fmt.Scanln(&asana_access)
+		fmt.Println("\nInput your Access Token: ")
+		fmt.Scan(&asana_access)
 		fmt.Println(b.Asana(asana_access))
 	} else if choose == 2 {
-		fmt.Println("\nInput your bing API key")
+		fmt.Print("\nInput your bing API key: ")
 		var bingapi string
-		fmt.Scanln(&bingapi)
+		fmt.Scan(&bingapi)
 		fmt.Println(b.Bing(bingapi))
 	} else if choose == 3 {
 		var access_token string
-		fmt.Println("\nInput your access token")
-		fmt.Scanln(&access_token)
+		fmt.Println("\nInput your access token: ")
+		fmt.Scan(&access_token)
 		fmt.Println(b.Bitly(access_token))
 	} else if choose == 4 {
 		var branch_key string
 		var branch_secret string
-		fmt.Println("\nInput your branch key")
-		fmt.Scanln(&branch_key)
-		fmt.Println("\nInput your branch secret")
-		fmt.Scanln(&branch_secret)
+		fmt.Println("\nInput your branch key: ")
+		fmt.Scan(&branch_key)
+		fmt.Println("\nInput your branch secret: ")
+		fmt.Scan(&branch_secret)
 		fmt.Println(b.Branch(branch_key, branch_secret))
 	} else if choose == 5 {
 		var buildkite_access string
-		fmt.Println("\nInput your Access Token")
-		fmt.Scanln(&buildkite_access)
+		fmt.Println("\nInput your Access Token: ")
+		fmt.Scan(&buildkite_access)
 		fmt.Println(b.Buildkite(buildkite_access))
 	} else if choose == 6 {
 		var butter_api string
-		fmt.Println("\nInput your API key")
-		fmt.Scanln(&butter_api)
+		fmt.Println("\nInput your API key: ")
+		fmt.Scan(&butter_api)
 		fmt.Println(b.ButterCMS(butter_api))
 	} else if choose == 7 {
 		var circleci_token string
-		fmt.Println("\nInput your CircleCI Access Token")
-		fmt.Scanln(&circleci_token)
+		fmt.Println("\nInput your CircleCI Access Token: ")
+		fmt.Scan(&circleci_token)
 		fmt.Println(b.Circleci(circleci_token))
 	} else if choose == 8 {
 		var dog_api string
 		var dog_application string
-		fmt.Println("\nInput your API key")
-		fmt.Scanln(&dog_api)
-		fmt.Println("\nInput your Application key")
-		fmt.Scanln(&dog_application)
+		fmt.Println("\nInput your API key: ")
+		fmt.Scan(&dog_api)
+		fmt.Println("\nInput your Application key: ")
+		fmt.Scan(&dog_application)
 		fmt.Println(b.Datadog(dog_api, dog_application))
 	} else if choose == 9 {
 		var loqate_key string
-		fmt.Println("\nInput your Loqate API key")
-		fmt.Scanln(&loqate_key)
+		fmt.Println("\nInput your Loqate API key: ")
+		fmt.Scan(&loqate_key)
 		fmt.Println(b.Loqate(loqate_key))
 	} else if choose == 10 {
 		var spotify_token string
-		fmt.Println("\nInput your Spotify Access Token")
-		fmt.Scanln(&spotify_token)
+		fmt.Println("\nInput your Spotify Access Token: ")
+		fmt.Scan(&spotify_token)
 		fmt.Println(b.Spotify(spotify_token))
 	} else if choose == 11 {
 		var travis_token string
-		fmt.Println("\nInput your Travis Token")
-		fmt.Scanln(&travis_token)
+		fmt.Println("\nInput your Travis Token: ")
+		fmt.Scan(&travis_token)
 		fmt.Println(b.Travis(travis_token))
 	} else if choose == 12 {
 		var wakatime_key string
-		fmt.Println("\nInput your Wakatime API Key")
-		fmt.Scanln(&wakatime_key)
+		fmt.Println("\nInput your Wakatime API Key: ")
+		fmt.Scan(&wakatime_key)
 		fmt.Println(b.Wakatime(wakatime_key))
 	}
 
