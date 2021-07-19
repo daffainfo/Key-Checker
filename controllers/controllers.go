932df248c5b9742b7a11032e9d6c986f7a84537c
Muhammad Daffa <36522826+daffainfo@users.noreply.github.com>
2021-07-19 10:50:31 +0700

    Adding 14 checkers

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -7,17 +7,23 @@ import (
 )
 
 //normal curl
-func normal_curl(url string, need_headers string, value_header string) (string, int) {
+func normal_curl(url string, need_headers int, set_header string, value_header string) (string, int) {
 	req, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if need_headers == "bearer" {
-		req.Header.Set("Authorization", "Bearer "+value_header)
-	} else if need_headers == "travis" {
-		req.Header.Set("Authorization", "Bearer "+value_header)
-		req.Header.Set("Authorization", "Bearer "+value_header)
+	if need_headers == 1 {
+		req.Header.Set(set_header, value_header)
+	} else if need_headers == 99 {
+		req.Header.Set(set_header, value_header)
+		req.Header.Set("Travis-API-Version", "3")
+	} else if need_headers == 98 {
+		req.Header.Set(set_header, value_header)
+		req.Header.Set("Accept", "application/vnd.heroku+json; version=3")
+	} else if need_headers == 97 {
+		req.Header.Set(set_header, value_header)
+		req.Header.Set("Accept", "application/vnd.pagerduty+json;version=2")
 	}
 
 	defer req.Body.Close()
@@ -31,9 +37,49 @@ func normal_curl(url string, need_headers string, value_header string) (string, 
 	return string(body), req.StatusCode
 }
 
+//basic auth
+func basic_auth(url string, username string, password string) int {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.SetBasicAuth(username, password)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	return resp.StatusCode
+}
+
+func Amplitude(amplitude_key string, amplitude_secret string) string {
+	url := "https://amplitude.com/api/2/export?start=20200201T5&end=20210203T20"
+	a := basic_auth(url, amplitude_key, amplitude_secret)
+
+	if a == 200 {
+		return "\nAmplitude API Keys valid"
+	} else {
+		return "\nAmplitude API Keys invalid"
+	}
+}
+
+func Asana(asana_token string) string {
+	url := "https://app.asana.com/api/1.0/users/me"
+	a, b := normal_curl(url, 1, "Authorization", "Bearer "+asana_token)
+	_ = a
+
+	if b == 200 {
+		return "\nAsana Access token valid"
+	} else {
+		return "\nAsana Access token invalid"
+	}
+}
+
 func Bing(bingapi string) string {
 	url := "https://dev.virtualearth.net/REST/v1/Locations?CountryRegion=US&adminDistrict=WA&locality=Somewhere&postalCode=98001&addressLine=100%20Main%20St.&key=" + bingapi
-	a, b := normal_curl(url, "", "")
+	a, b := normal_curl(url, 0, "", "")
 	_ = a
 
 	if b == 200 {
@@ -46,7 +92,7 @@ func Bing(bingapi string) string {
 func Bitly(bitly_token string) string {
 	error_msg := "{\"status_code\":500,\"status_txt\":\"INVALID_ARG_ACCESS_TOKEN\",\"data\":[]}"
 	url := "https://api-ssl.bitly.com/v3/shorten?access_token=" + bitly_token + "&longUrl=https://www.google.com"
-	a, b := normal_curl(url, "", "")
+	a, b := normal_curl(url, 0, "", "")
 
 	if a != error_msg && b == 200 {
 		return "\nBit.ly Access token valid"
@@ -57,47 +103,66 @@ func Bitly(bitly_token string) string {
 
 func Branch(branch_key string, branch_secret string) string {
 	url := "https://api2.branch.io/v1/app/" + branch_key + "?branch_secret=" + branch_secret
-
-	a, b := normal_curl(url, "", "")
+	a, b := normal_curl(url, 0, "", "")
 	_ = a
 
 	if b == 200 {
 		return "\nBranch.IO Key and Secret valid"
 	} else {
 		return "\nBranch.IO Key and Secret invalid"
+	}
+}
+
+func Browserstack(browserstack_username string, browserstack_key string) string {
+	url := "https://api.browserstack.com/automate/plan.json"
+	a := basic_auth(url, browserstack_username, browserstack_key)
+
+	if a == 200 {
+		return "\nBrowserStack Access Key valid"
+	} else {
+		return "\nBrowserStack Access Key invalid"
 	}
 }
 
 func Buildkite(buildkite_access string) string {
 	url := "https://api.buildkite.com/v2/user"
-
-	a, b := normal_curl(url, "bearer", buildkite_access)
+	a, b := normal_curl(url, 1, "Authorization", "Bearer "+buildkite_access)
 	_ = a
 
 	if b == 200 {
-		return "\nAsana Access token valid"
+		return "\nBuildkite Access token valid"
 	} else {
-		return "\nAsana Access token invalid"
+		return "\nBuildkite Access token invalid"
 	}
 }
 
 func ButterCMS(branch_key string) string {
 	url := "https://api.buttercms.com/v2/posts/?auth_token=" + branch_key
-
-	a, b := normal_curl(url, "", "")
+	a, b := normal_curl(url, 0, "", "")
 	_ = a
 
 	if b == 200 {
-		return "\nBranch.IO Key and Secret valid"
+		return "\nButterCMS API Key valid"
 	} else {
-		return "\nBranch.IO Key and Secret invalid"
+		return "\nButterCMS API Key invalid"
+	}
+}
+
+func Calendly(calendly_key string) string {
+	url := "https://calendly.com/api/v1/users/me"
+	a, b := normal_curl(url, 1, "X-TOKEN", calendly_key)
+	_ = a
+
+	if b == 200 {
+		return "\nCalendly API Key valid"
+	} else {
+		return "\nCalendly API Key invalid"
 	}
 }
 
 func Circleci(circleci_token string) string {
 	url := "https://circleci.com/api/v1.1/me?circle-token=" + circleci_token
-
-	a, b := normal_curl(url, "", "")
+	a, b := normal_curl(url, 0, "", "")
 	_ = a
 
 	if b == 200 {
@@ -109,8 +174,7 @@ func Circleci(circleci_token string) string {
 
 func Datadog(dog_api string, dog_application string) string {
 	url := "https://api.datadoghq.com/api/v1/dashboard?api_key=" + dog_api + "&application_key=" + dog_application
-
-	a, b := normal_curl(url, "", "")
+	a, b := normal_curl(url, 0, "", "")
 	_ = a
 
 	if b == 200 {
@@ -122,8 +186,7 @@ func Datadog(dog_api string, dog_application string) string {
 
 func Facebook(facebook_token string) string {
 	url := "https://developers.facebook.com/tools/debug/accesstoken/?access_token=" + facebook_token + "&version=v3.2"
-
-	a, b := normal_curl(url, "", "")
+	a, b := normal_curl(url, 0, "", "")
 	_ = a
 
 	if b == 200 {
@@ -133,48 +196,269 @@ func Facebook(facebook_token string) string {
 	}
 }
 
-func Loqate(loqate_key string) string {
-	error_msg := "{\"Items\":[{\"Error\":\"2\",\"Description\":\"Unknown key\",\"Cause\":\"The key you are using to access the service was not found.\",\"Resolution\":\"Please check that the key is correct. It should be in the form AA11-AA11-AA11-AA11.\"}]}"
-	url := "http://api.addressy.com/Capture/Interactive/Find/v1.00/json3.ws?Key=" + loqate_key + "&Countries=US,CA&Language=en&Limit=5&Text=BHAR"
+func Github(github_username string, github_token string) string {
+	url := "https://api.github.com/user"
+	a := basic_auth(url, github_username, github_token)
 
-	a, b := normal_curl(url, "", "")
+	if a == 200 {
+		return "\nGithub Token valid"
+	} else {
+		return "\nGithub Token invalid"
+	}
+}
+
+func Heroku(heroku_key string) string {
+	url := "https://api.heroku.com/apps"
+	a, b := normal_curl(url, 98, "Authorization", "Bearer "+heroku_key)
+	_ = a
+
+	if b == 200 {
+		return "\nHeroku API Key valid"
+	} else {
+		return "\nHeroku API Key invalid"
+	}
+}
+
+func Hubspot(hubspot_key string) string {
+	url := "https://api.hubapi.com/owners/v2/owners?hapikey=" + hubspot_key
+	a, b := normal_curl(url, 0, "", "")
+	_ = a
+
+	if b == 200 {
+		return "\nHubspot API Key valid"
+	} else {
+		return "\nHubspot API Key invalid"
+	}
+}
+
+func Ipstack(ipstack_key string) string {
+	error_msg := "{\"success\":false,\"error\":{\"code\":101,\"type\":\"invalid_access_key\",\"info\":\"You have not supplied a valid API Access Key. [Technical Support: support@apilayer.com]\"}}"
+	url := "https://api.ipstack.com/134.201.250.155?access_key=" + ipstack_key
+
+	a, b := normal_curl(url, 0, "", "")
 	_ = b
 
 	if a != error_msg {
-		return "\nDataDog API key valid"
+		return "\nIpstack API Key valid"
 	} else {
-		return "\nDataDog API key invalid"
+		return "\nIpstack API Key invalid"
+	}
+}
+
+func Iterable(iterable_key string) string {
+	url := "https://api.iterable.com/api/export/data.json?dataTypeName=emailSend&range=Today&onlyFields=List.empty"
+	a, b := normal_curl(url, 1, "Api_Key", iterable_key)
+	_ = a
+
+	if b == 200 {
+		return "\nIterable API Key valid"
+	} else {
+		return "\nIterable API Key invalid"
+	}
+}
+
+func Jumpcloud(jumpcloud_key string) string {
+	url := "https://console.jumpcloud.com/api/v2/systemgroups"
+	a, b := normal_curl(url, 1, "x-api-key", jumpcloud_key)
+	_ = a
+
+	if b == 200 {
+		return "\nJumpCloud API Key valid"
+	} else {
+		return "\nJumpCloud API Key invalid"
+	}
+}
+
+func Lokalise(lokalise_key string) string {
+	url := "https://api.lokalise.com/api2/projects/"
+	a, b := normal_curl(url, 1, "X-Api-Token", lokalise_key)
+	_ = a
+
+	if b == 200 {
+		return "\nLokalise API key valid"
+	} else {
+		return "\nLokalise API key invalid"
+	}
+}
+
+func Loqate(loqate_key string) string {
+	error_msg := "{\"Items\":[{\"Error\":\"2\",\"Description\":\"Unknown key\",\"Cause\":\"The key you are using to access the service was not found.\",\"Resolution\":\"Please check that the key is correct. It should be in the form AA11-AA11-AA11-AA11.\"}]}"
+	url := "http://api.addressy.com/Capture/Interactive/Find/v1.00/json3.ws?Key=" + loqate_key + "&Countries=US,CA&Language=en&Limit=5&Text=BHAR"
+	a, b := normal_curl(url, 0, "", "")
+	_ = b
+
+	if a != error_msg {
+		return "\nLoqate API key valid"
+	} else {
+		return "\nLoqate API key invalid"
+	}
+}
+func Mailgun(mailgun_key string) string {
+	url := "https://api.mailgun.net/v3/domains"
+	a := basic_auth(url, "api", mailgun_key)
+
+	if a == 200 {
+		return "\nMailgun API key valid"
+	} else {
+		return "\nMailgun API key invalid"
+	}
+}
+
+func Mapbox(mapbox_key string) string {
+	url := "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=" + mapbox_key
+	a, b := normal_curl(url, 0, "", "")
+	_ = a
+
+	if b == 200 {
+		return "\nMapbox API key valid"
+	} else {
+		return "\nMapbox API key invalid"
+	}
+}
+
+func Pagerduty(pagerduty_key string) string {
+	url := "https://api.pagerduty.com/schedules"
+	a, b := normal_curl(url, 97, "Authorization", "Token token="+pagerduty_key)
+	_ = a
+
+	if b == 200 {
+		return "\nPagerduty API Key valid"
+	} else {
+		return "\nPagerduty API Key invalid"
+	}
+}
+
+func Pendo(pendo_key string) string {
+	url := "https://app.pendo.io/api/v1/feature"
+	a, b := normal_curl(url, 1, "x-pendo-integration-key", pendo_key)
+	_ = a
+
+	if b == 200 {
+		return "\nPendo Integration Key valid"
+	} else {
+		return "\nPendo Integration Key invalid"
+	}
+}
+
+func Pivotaltracker(pivotaltracker_token string) string {
+	url := "https://www.pivotaltracker.com/services/v5/stories/555"
+	a, b := normal_curl(url, 1, "X-TrackerToken", pivotaltracker_token)
+	_ = a
+
+	if b == 200 {
+		return "\nPivotalTracker API Token valid"
+	} else {
+		return "\nPivotalTracker API Token invalid"
+	}
+}
+
+func Razorpay(razor_key string, razor_secret string) string {
+	url := "https://api.razorpay.com/v1/payments"
+	a := basic_auth(url, razor_key, razor_secret)
+
+	if a == 200 {
+		return "\nRazorpay API key and Secret key valid"
+	} else {
+		return "\nRazorpay API key and Secret key invalid"
+	}
+}
+
+func Sauce_labs(sauce_username string, sauce_key string) string {
+	url := "https://saucelabs.com/rest/v1/users/" + sauce_username
+	a := basic_auth(url, sauce_username, sauce_key)
+
+	if a == 200 {
+		return "\nSauceLabs Username and access Key valid"
+	} else {
+		return "\nSauceLabs Username and access Key invalid"
+	}
+}
+
+func Sendgrid(sendgrid_token string) string {
+	url := "https://api.sendgrid.com/v3/scopes"
+	a, b := normal_curl(url, 1, "Authorization", "Bearer "+sendgrid_token)
+	_ = a
+
+	if b == 200 {
+		return "\nSendGrid API Token valid"
+	} else {
+		return "\nSendGrid API Token invalid"
 	}
 }
 
 func Spotify(spotify_token string) string {
 	url := "https://api.spotify.com/v1/me"
-	a, b := normal_curl(url, "bearer", spotify_token)
+	a, b := normal_curl(url, 1, "Authorization", "Bearer "+spotify_token)
 	_ = a
 
 	if b == 200 {
 		return "\nSpotify Access Token valid"
 	} else {
 		return "\nSpotify Access Token invalid"
+	}
+}
+
+func Stripe(stripe_token string) string {
+	url := "https://api.stripe.com/v1/charges"
+	a := basic_auth(url, stripe_token, "")
+
+	if a == 200 {
+		return "\nStripe API key valid"
+	} else {
+		return "\nStripe API key invalid"
 	}
 }
 
 func Travis(travis_token string) string {
 	url := "https://api.travis-ci.org/repos"
-
-	a, b := normal_curl(url, "bearer", travis_token)
+	a, b := normal_curl(url, 99, "Authorization", "Bearer "+travis_token)
 	_ = a
 
 	if b == 200 {
-		return "\nSpotify Access Token valid"
+		return "\nTravis CI API token valid"
 	} else {
-		return "\nSpotify Access Token invalid"
+		return "\nTravis CI API token invalid"
+	}
+}
+
+func Twilio(twilio_sid string, twilio_token string) string {
+	url := "https://api.twilio.com/2010-04-01/Accounts.json"
+	a := basic_auth(url, twilio_sid, twilio_token)
+
+	if a == 200 {
+		return "\nTwilio Account_sid and Auth token valid"
+	} else {
+		return "\nTwilio Account_sid and Auth token invalid"
+	}
+}
+
+func Appcenter(appcenter_token string) string {
+	url := "https://api.appcenter.ms/v0.1/apps"
+	a, b := normal_curl(url, 1, "X-Api-Token", appcenter_token)
+	_ = a
+
+	if b == 200 {
+		return "\nVisual Studio App Center API Token valid"
+	} else {
+		return "\nVisual Studio App Center API Token invalid"
+	}
+}
+
+func Youtube(youtube_key string) string {
+	url := "https://www.googleapis.com/youtube/v3/activities?part=contentDetails&maxResults=25&channelId=UC-lHJZR3Gqxm24_Vd_AJ5Yw&key=" + youtube_key
+	a, b := normal_curl(url, 0, "", "")
+	_ = a
+
+	if b == 200 {
+		return "\nYoutube API Key valid"
+	} else {
+		return "\nYoutube API Key invalid"
 	}
 }
 
 func Wakatime(wakatime_key string) string {
 	url := "https://wakatime.com/api/v1/users/current/projects/?api_key=" + wakatime_key
-	a, b := normal_curl(url, "", "")
+	a, b := normal_curl(url, 0, "", "")
 	_ = a
 
 	if b == 200 {
@@ -184,15 +468,26 @@ func Wakatime(wakatime_key string) string {
 	}
 }
 
-func Youtube(youtube_key string) string {
-	url := "https://www.googleapis.com/youtube/v3/activities?part=contentDetails&maxResults=25&channelId=UC-lHJZR3Gqxm24_Vd_AJ5Yw&key=" + youtube_key
-
-	a, b := normal_curl(url, "", "")
+func WPEngine(wp_account string, wp_key string) string {
+	url := "https://api.wpengine.com/1.2/?method=site&account_name=" + wp_account + "&wpe_apikey=" + wp_key
+	a, b := normal_curl(url, 0, "", "")
 	_ = a
 
 	if b == 200 {
-		return "\nYoutube API Key valid"
+		return "\nWPEngine API Key valid"
 	} else {
-		return "\nYoutube API Key invalid"
+		return "\nWPEngine API Key invalid"
+	}
+}
+
+func Zendesk(zendesk_subdomain string, zendesk_token string) string {
+	url := "https://" + zendesk_subdomain + ".zendesk.com/api/v2/tickets.json"
+	a, b := normal_curl(url, 1, "Authorization", "Bearer "+zendesk_token)
+	_ = a
+
+	if b == 200 {
+		return "\nZendesk Acces Token valid"
+	} else {
+		return "\nZendesk Acces Token invalid"
 	}
 }
--- main.go
+++ main.go
@@ -19,89 +19,261 @@ func main() {
 
   `)
 	fmt.Println("List of API Checker")
-	fmt.Println("1. Bing Maps API Key")
-	fmt.Println("2. Bit.ly Access token")
-	fmt.Println("3. Branch.IO Key and Secret")
-	fmt.Println("4. Buildkite Access token")
-	fmt.Println("5. ButterCMS API Key")
-	fmt.Println("6. CircleCI Access Token")
-	fmt.Println("7. DataDog API key")
-	fmt.Println("8. Facebook Access Token")
-	fmt.Println("9. Loqate API key")
-	fmt.Println("10. Spotify Access Token")
-	fmt.Println("11. Travis CI API token")
-	fmt.Println("12. Youtube API Key")
-	fmt.Println("13. WakaTime API Key")
+	fmt.Println("1. Amplitude API Key")
+	fmt.Println("2. Asana Access token")
+	fmt.Println("3. Bing Maps API Key")
+	fmt.Println("4. Bit.ly Access token")
+	fmt.Println("5. Branch.IO Key and Secret")
+	fmt.Println("6. BrowserStack Access Key")
+	fmt.Println("7. Buildkite Access token")
+	fmt.Println("8. ButterCMS API Key")
+	fmt.Println("9. Calendly API Key")
+	fmt.Println("10. CircleCI Access Token")
+	fmt.Println("11. DataDog API key")
+	fmt.Println("12. Facebook Access Token")
+	fmt.Println("13. Github Token")
+	fmt.Println("14. Heroku API Key")
+	fmt.Println("15. Hubspot API Key")
+	fmt.Println("16. Ipstack API Key")
+	fmt.Println("17. Iterable API Key")
+	fmt.Println("18. Jumpcloud API key")
+	fmt.Println("19. Lokalise API Key")
+	fmt.Println("20. Loqate API key")
+	fmt.Println("21. MailGun API Key")
+	fmt.Println("22. Mapbox API Key")
+	fmt.Println("23. Pagerduty API token")
+	fmt.Println("24. Pendo Integration Key")
+	fmt.Println("25. PivotalTracker API Token")
+	fmt.Println("26. Razorpay API key and secret key")
+	fmt.Println("27. SauceLabs Username and access Key")
+	fmt.Println("28. SendGrid API Token")
+	fmt.Println("29. Spotify Access Token")
+	fmt.Println("30. Stripe Live Token")
+	fmt.Println("31. Travis CI API token")
+	fmt.Println("32. Twilio Account_sid and Auth token")
+	fmt.Println("33. Visual Studio App Center API Token")
+	fmt.Println("34. Youtube API Key")
+	fmt.Println("35. WakaTime API Key")
+	fmt.Println("36. WPEngine API Key")
+	fmt.Println("37. Zendesk Access token")
 
 	fmt.Println("\nChoose checkers:")
 	fmt.Scan(&choose)
 
 	if choose == 1 {
+		var amplitude_key string
+		var amplitude_secret string
+		fmt.Print("\nInput your Amplitude Key: ")
+		fmt.Scan(&amplitude_key)
+		fmt.Print("\nInput your Amplitude Secret: ")
+		fmt.Scan(&amplitude_secret)
+		fmt.Println(b.Amplitude(amplitude_key, amplitude_secret))
+	} else if choose == 2 {
+		fmt.Print("\nInput your Asana Access token: ")
+		var asana_token string
+		fmt.Scan(&asana_token)
+		fmt.Println(b.Asana(asana_token))
+	} else if choose == 3 {
 		fmt.Print("\nInput your bing API key: ")
 		var bingapi string
 		fmt.Scan(&bingapi)
 		fmt.Println(b.Bing(bingapi))
-	} else if choose == 2 {
+	} else if choose == 4 {
 		var access_token string
-		fmt.Println("\nInput your access token: ")
+		fmt.Print("\nInput your Bit.ly Access token: ")
 		fmt.Scan(&access_token)
 		fmt.Println(b.Bitly(access_token))
-	} else if choose == 3 {
+	} else if choose == 5 {
 		var branch_key string
 		var branch_secret string
-		fmt.Println("\nInput your branch key: ")
+		fmt.Print("\nInput your branch key: ")
 		fmt.Scan(&branch_key)
-		fmt.Println("\nInput your branch secret: ")
+		fmt.Print("\nInput your branch secret: ")
 		fmt.Scan(&branch_secret)
 		fmt.Println(b.Branch(branch_key, branch_secret))
-	} else if choose == 4 {
+	} else if choose == 6 {
+		var browserstack_username string
+		var browserstack_key string
+		fmt.Print("\nInput your BrowserStack Username: ")
+		fmt.Scan(&browserstack_username)
+		fmt.Print("\nInput your BrowserStack Key: ")
+		fmt.Scan(&browserstack_key)
+		fmt.Println(b.Browserstack(browserstack_username, browserstack_key))
+	} else if choose == 7 {
 		var buildkite_access string
-		fmt.Println("\nInput your Access Token: ")
+		fmt.Print("\nInput your Buildkite Access token: ")
 		fmt.Scan(&buildkite_access)
 		fmt.Println(b.Buildkite(buildkite_access))
-	} else if choose == 5 {
+	} else if choose == 8 {
 		var butter_api string
-		fmt.Println("\nInput your API key: ")
+		fmt.Print("\nInput your ButterCMS API Key: ")
 		fmt.Scan(&butter_api)
 		fmt.Println(b.ButterCMS(butter_api))
-	} else if choose == 6 {
+	} else if choose == 9 {
+		var calendly_key string
+		fmt.Print("\nInput your Calendly API Key: ")
+		fmt.Scan(&calendly_key)
+		fmt.Println(b.Calendly(calendly_key))
+	} else if choose == 10 {
 		var circleci_token string
-		fmt.Println("\nInput your CircleCI Access Token: ")
+		fmt.Print("\nInput your CircleCI Access Token: ")
 		fmt.Scan(&circleci_token)
 		fmt.Println(b.Circleci(circleci_token))
-	} else if choose == 7 {
+	} else if choose == 11 {
 		var dog_api string
 		var dog_application string
-		fmt.Println("\nInput your API key: ")
+		fmt.Print("\nInput your Datadog API key: ")
 		fmt.Scan(&dog_api)
-		fmt.Println("\nInput your Application key: ")
+		fmt.Print("\nInput your Datadog Application key: ")
 		fmt.Scan(&dog_application)
 		fmt.Println(b.Datadog(dog_api, dog_application))
-	} else if choose == 8 {
+	} else if choose == 12 {
+		var facebook_token string
+		fmt.Println("\nInput your Facebook Access Token: ")
+		fmt.Scan(&facebook_token)
+		fmt.Println(b.Facebook(facebook_token))
+	} else if choose == 13 {
+		var github_username string
+		var github_token string
+		fmt.Print("\nInput your Github Username: ")
+		fmt.Scan(&github_username)
+		fmt.Print("\nInput your Github Access Token: ")
+		fmt.Scan(&github_token)
+		fmt.Println(b.Github(github_username, github_token))
+	} else if choose == 14 {
+		var heroku_key string
+		fmt.Print("\nInput your Heroku API Key: ")
+		fmt.Scan(&heroku_key)
+		fmt.Println(b.Heroku(heroku_key))
+	} else if choose == 15 {
+		var hubspot_key string
+		fmt.Print("\nInput your Hubspot API Key: ")
+		fmt.Scan(&hubspot_key)
+		fmt.Println(b.Hubspot(hubspot_key))
+	} else if choose == 16 {
+		var ipstack_key string
+		fmt.Print("\nInput your Ipstack API Key: ")
+		fmt.Scan(&ipstack_key)
+		fmt.Println(b.Ipstack(ipstack_key))
+	} else if choose == 17 {
+		var iterable_key string
+		fmt.Print("\nInput your Iterable API Key: ")
+		fmt.Scan(&iterable_key)
+		fmt.Println(b.Iterable(iterable_key))
+	} else if choose == 18 {
+		var jumpcloud_key string
+		fmt.Print("\nInput your JumpCloud API Key: ")
+		fmt.Scan(&jumpcloud_key)
+		fmt.Println(b.Jumpcloud(jumpcloud_key))
+	} else if choose == 19 {
+		var lokalise_key string
+		fmt.Print("\nInput your Lokalise API key: ")
+		fmt.Scan(&lokalise_key)
+		fmt.Println(b.Lokalise(lokalise_key))
+	} else if choose == 20 {
 		var loqate_key string
-		fmt.Println("\nInput your Loqate API key: ")
+		fmt.Print("\nInput your Loqate API key: ")
 		fmt.Scan(&loqate_key)
 		fmt.Println(b.Loqate(loqate_key))
-	} else if choose == 9 {
+	} else if choose == 21 {
+		var mailgun_key string
+		fmt.Print("\nInput your Mailgun API key: ")
+		fmt.Scan(&mailgun_key)
+		fmt.Println(b.Mailgun(mailgun_key))
+	} else if choose == 22 {
+		var mapbox_key string
+		fmt.Print("\nInput your Mapbox API key: ")
+		fmt.Scan(&mapbox_key)
+		fmt.Println(b.Mapbox(mapbox_key))
+	} else if choose == 23 {
+		var pagerduty_key string
+		fmt.Print("\nInput your Pagerduty API token: ")
+		fmt.Scan(&pagerduty_key)
+		fmt.Println(b.Pagerduty(pagerduty_key))
+	} else if choose == 24 {
+		var pendo_key string
+		fmt.Print("\nInput your Pendo Integration Key: ")
+		fmt.Scan(&pendo_key)
+		fmt.Println(b.Pendo(pendo_key))
+	} else if choose == 25 {
+		var pivotaltracker_token string
+		fmt.Print("\nInput your PivotalTracker API Token: ")
+		fmt.Scan(&pivotaltracker_token)
+		fmt.Println(b.Pivotaltracker(pivotaltracker_token))
+	} else if choose == 26 {
+		var razor_key string
+		var razor_secret string
+		fmt.Print("\nInput your Razor Key: ")
+		fmt.Scan(&razor_key)
+		fmt.Print("\nInput your Razor Secret: ")
+		fmt.Scan(&razor_secret)
+		fmt.Println(b.Razorpay(razor_key, razor_secret))
+	} else if choose == 27 {
+		var sauce_username string
+		var sauce_key string
+		fmt.Print("\nInput your SauceLabs Username: ")
+		fmt.Scan(&sauce_username)
+		fmt.Print("\nInput your SauceLabs Access Token: ")
+		fmt.Scan(&sauce_key)
+		fmt.Println(b.Sauce_labs(sauce_username, sauce_key))
+	} else if choose == 28 {
+		var sendgrid_token string
+		fmt.Print("\nInput your SendGrid API Token: ")
+		fmt.Scan(&sendgrid_token)
+		fmt.Println(b.Sendgrid(sendgrid_token))
+	} else if choose == 29 {
 		var spotify_token string
-		fmt.Println("\nInput your Spotify Access Token: ")
+		fmt.Print("\nInput your Spotify Access Token: ")
 		fmt.Scan(&spotify_token)
 		fmt.Println(b.Spotify(spotify_token))
-	} else if choose == 10 {
+	} else if choose == 30 {
+		var stripe_token string
+		fmt.Print("\nInput your Stripe Live Token: ")
+		fmt.Scan(&stripe_token)
+		fmt.Println(b.Stripe(stripe_token))
+	} else if choose == 31 {
 		var travis_token string
-		fmt.Println("\nInput your Travis Token: ")
+		fmt.Print("\nInput your Travis Token: ")
 		fmt.Scan(&travis_token)
 		fmt.Println(b.Travis(travis_token))
-	} else if choose == 11 {
+	} else if choose == 32 {
+		var twilio_sid string
+		var twilio_token string
+		fmt.Print("\nInput your Twilio SID: ")
+		fmt.Scan(&twilio_sid)
+		fmt.Print("\nInput your Twilio Token: ")
+		fmt.Scan(&twilio_token)
+		fmt.Println(b.Twilio(twilio_sid, twilio_token))
+	} else if choose == 33 {
+		var appcenter_token string
+		fmt.Print("\nInput your Visual Studio App Center API Token: ")
+		fmt.Scan(&appcenter_token)
+		fmt.Println(b.Appcenter(appcenter_token))
+	} else if choose == 34 {
 		var youtube_key string
-		fmt.Println("\nInput your Youtube API Key: ")
+		fmt.Print("\nInput your Youtube API Key: ")
 		fmt.Scan(&youtube_key)
 		fmt.Println(b.Youtube(youtube_key))
-	} else if choose == 12 {
+	} else if choose == 35 {
 		var wakatime_key string
-		fmt.Println("\nInput your Wakatime API Key: ")
+		fmt.Print("\nInput your Wakatime API Key: ")
 		fmt.Scan(&wakatime_key)
 		fmt.Println(b.Wakatime(wakatime_key))
+	} else if choose == 36 {
+		var wp_account string
+		var wp_key string
+		fmt.Print("\nInput your WPEngine Account: ")
+		fmt.Scan(&wp_account)
+		fmt.Print("\nInput your WPEngine Key: ")
+		fmt.Scan(&wp_key)
+		fmt.Println(b.WPEngine(wp_account, wp_key))
+	} else if choose == 37 {
+		var zendesk_subdomain string
+		var zendesk_token string
+		fmt.Print("\nInput Zendesk Subdomain: ")
+		fmt.Scan(&zendesk_subdomain)
+		fmt.Print("\nInput Zendesk Token: ")
+		fmt.Scan(&zendesk_token)
+		fmt.Println(b.Zendesk(zendesk_subdomain, zendesk_token))
 	}
-
 }
