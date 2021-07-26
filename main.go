08ef824ed97985540101f9a8b579e722b69a69a7
daffainfo <md15ev@gmail.com>
2021-07-26 23:57:27 +0700

    Fix minor bug

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 
 	b "github.com/daffainfo/Key-Checker/controllers"
 )
@@ -21,7 +19,6 @@ const (
 var (
 	input_key  = ""
 	input_key2 = ""
-	arr        []string
 )
 
 type JsonType struct {
@@ -42,15 +39,6 @@ Version = 1.0.0
 
 }
 
-func show_list() {
-	file, _ := ioutil.ReadFile("data.json")
-	_ = json.Unmarshal([]byte(file), &arr)
-
-	for i, checkers := range arr {
-		fmt.Printf(green+"%d. %s%s\n", i+1, checkers, white)
-	}
-}
-
 func ask_singlekey(choose_arr string) string {
 	fmt.Printf(blue+"\nYou choose %s checker", choose_arr)
 	fmt.Printf("\nInput your %s:%s\n", choose_arr, white)
@@ -69,96 +57,137 @@ func ask_secondkey(choose_arr string, question1 string, question2 string) (strin
 
 func main() {
 	var choose int
+	listname := []string{
+		"Amplitude API Key",
+		"Asana Access token",
+		"Bing Maps API Key",
+		"Bit.ly Access token",
+		"Branch.IO Key and Secret",
+		"BrowserStack Access Key",
+		"Buildkite Access token",
+		"ButterCMS API Key",
+		"Calendly API Key",
+		"CircleCI Access Token",
+		"DataDog API key",
+		"Facebook Access Token",
+		"Github Token",
+		"Heroku API Key",
+		"Hubspot API Key",
+		"Ipstack API Key",
+		"Iterable API Key",
+		"Jumpcloud API key",
+		"Lokalise API Key",
+		"Loqate API key",
+		"MailGun API Key",
+		"Mapbox API Key",
+		"Pagerduty API token",
+		"Pendo Integration Key",
+		"PivotalTracker API Token",
+		"Razorpay API key and secret key",
+		"SauceLabs Username and access Key",
+		"SendGrid API Token",
+		"Spotify Access Token",
+		"Stripe Live Token",
+		"Travis CI API token",
+		"Twilio Account_sid and Auth token",
+		"Visual Studio App Center API Token",
+		"Youtube API Key",
+		"WakaTime API Key",
+		"WPEngine API Key",
+		"Zendesk Access token"}
 
 	show_banner()
-	show_list()
+
+	for i, checkers := range listname {
+		fmt.Printf(green+"%d. %s%s\n", i+1, checkers, white)
+	}
 
 	fmt.Printf("%s\nChoose checkers: ", green)
 	fmt.Scan(&choose)
 
 	if choose == 1 {
-		q1, q2 := ask_secondkey(arr[0], "Amplitude Key", "Amplitude Secret")
+		q1, q2 := ask_secondkey(listname[0], "Amplitude Key", "Amplitude Secret")
 		fmt.Println(b.Amplitude(q1, q2))
 	} else if choose == 2 {
-		fmt.Println(b.Asana(ask_singlekey(arr[1])))
+		fmt.Println(b.Asana(ask_singlekey(listname[1])))
 	} else if choose == 3 {
-		fmt.Println(b.Bing(ask_singlekey(arr[2])))
+		fmt.Println(b.Bing(ask_singlekey(listname[2])))
 	} else if choose == 4 {
-		fmt.Println(b.Bitly(ask_singlekey(arr[3])))
+		fmt.Println(b.Bitly(ask_singlekey(listname[3])))
 	} else if choose == 5 {
-		q1, q2 := ask_secondkey(arr[4], "Branch Key", "Branch Secret")
+		q1, q2 := ask_secondkey(listname[4], "Branch Key", "Branch Secret")
 		fmt.Println(b.Branch(q1, q2))
 	} else if choose == 6 {
-		q1, q2 := ask_secondkey(arr[5], "BrowserStack Username", "BrowserStack Key")
+		q1, q2 := ask_secondkey(listname[5], "BrowserStack Username", "BrowserStack Key")
 		fmt.Println(b.Browserstack(q1, q2))
 	} else if choose == 7 {
-		fmt.Println(b.Buildkite(ask_singlekey(arr[6])))
+		fmt.Println(b.Buildkite(ask_singlekey(listname[6])))
 	} else if choose == 8 {
-		fmt.Println(b.Buildkite(ask_singlekey(arr[7])))
+		fmt.Println(b.Buildkite(ask_singlekey(listname[7])))
 	} else if choose == 9 {
-		fmt.Println(b.Calendly(ask_singlekey(arr[8])))
+		fmt.Println(b.Calendly(ask_singlekey(listname[8])))
 	} else if choose == 10 {
-		fmt.Println(b.Circleci(ask_singlekey(arr[9])))
+		fmt.Println(b.Circleci(ask_singlekey(listname[9])))
 	} else if choose == 11 {
-		q1, q2 := ask_secondkey(arr[10], "Datadog API key", "Datadog Application key")
+		q1, q2 := ask_secondkey(listname[10], "Datadog API key", "Datadog Application key")
 		fmt.Println(b.Datadog(q1, q2))
 	} else if choose == 12 {
-		fmt.Println(b.Facebook(ask_singlekey(arr[11])))
+		fmt.Println(b.Facebook(ask_singlekey(listname[11])))
 	} else if choose == 13 {
-		q1, q2 := ask_secondkey(arr[12], "Github Username", "Github Access Token")
+		q1, q2 := ask_secondkey(listname[12], "Github Username", "Github Access Token")
 		fmt.Println(b.Github(q1, q2))
 	} else if choose == 14 {
-		fmt.Println(b.Heroku(ask_singlekey(arr[13])))
+		fmt.Println(b.Heroku(ask_singlekey(listname[13])))
 	} else if choose == 15 {
-		fmt.Println(b.Hubspot(ask_singlekey(arr[14])))
+		fmt.Println(b.Hubspot(ask_singlekey(listname[14])))
 	} else if choose == 16 {
-		fmt.Println(b.Ipstack(ask_singlekey(arr[15])))
+		fmt.Println(b.Ipstack(ask_singlekey(listname[15])))
 	} else if choose == 17 {
-		fmt.Println(b.Iterable(ask_singlekey(arr[16])))
+		fmt.Println(b.Iterable(ask_singlekey(listname[16])))
 	} else if choose == 18 {
-		fmt.Println(b.Jumpcloud(ask_singlekey(arr[17])))
+		fmt.Println(b.Jumpcloud(ask_singlekey(listname[17])))
 	} else if choose == 19 {
-		fmt.Println(b.Lokalise(ask_singlekey(arr[18])))
+		fmt.Println(b.Lokalise(ask_singlekey(listname[18])))
 	} else if choose == 20 {
-		fmt.Println(b.Loqate(ask_singlekey(arr[19])))
+		fmt.Println(b.Loqate(ask_singlekey(listname[19])))
 	} else if choose == 21 {
-		fmt.Println(b.Mailgun(ask_singlekey(arr[20])))
+		fmt.Println(b.Mailgun(ask_singlekey(listname[20])))
 	} else if choose == 22 {
-		fmt.Println(b.Mapbox(ask_singlekey(arr[21])))
+		fmt.Println(b.Mapbox(ask_singlekey(listname[21])))
 	} else if choose == 23 {
-		fmt.Println(b.Pagerduty(ask_singlekey(arr[22])))
+		fmt.Println(b.Pagerduty(ask_singlekey(listname[22])))
 	} else if choose == 24 {
-		fmt.Println(b.Pendo(ask_singlekey(arr[23])))
+		fmt.Println(b.Pendo(ask_singlekey(listname[23])))
 	} else if choose == 25 {
-		fmt.Println(b.Pivotaltracker(ask_singlekey(arr[24])))
+		fmt.Println(b.Pivotaltracker(ask_singlekey(listname[24])))
 	} else if choose == 26 {
-		q1, q2 := ask_secondkey(arr[25], "Razor Key", "Github Razor Secret")
+		q1, q2 := ask_secondkey(listname[25], "Razor Key", "Github Razor Secret")
 		fmt.Println(b.Razorpay(q1, q2))
 	} else if choose == 27 {
-		q1, q2 := ask_secondkey(arr[26], "SauceLabs Username", "Access Token")
+		q1, q2 := ask_secondkey(listname[26], "SauceLabs Username", "Access Token")
 		fmt.Println(b.Sauce_labs(q1, q2))
 	} else if choose == 28 {
-		fmt.Println(b.Sendgrid(ask_singlekey(arr[27])))
+		fmt.Println(b.Sendgrid(ask_singlekey(listname[27])))
 	} else if choose == 29 {
-		fmt.Println(b.Spotify(ask_singlekey(arr[28])))
+		fmt.Println(b.Spotify(ask_singlekey(listname[28])))
 	} else if choose == 30 {
-		fmt.Println(b.Stripe(ask_singlekey(arr[29])))
+		fmt.Println(b.Stripe(ask_singlekey(listname[29])))
 	} else if choose == 31 {
-		fmt.Println(b.Travis(ask_singlekey(arr[30])))
+		fmt.Println(b.Travis(ask_singlekey(listname[30])))
 	} else if choose == 32 {
-		q1, q2 := ask_secondkey(arr[31], "Twilio SID", "Twilio Token")
+		q1, q2 := ask_secondkey(listname[31], "Twilio SID", "Twilio Token")
 		fmt.Println(b.Twilio(q1, q2))
 	} else if choose == 33 {
-		fmt.Println(b.Appcenter(ask_singlekey(arr[32])))
+		fmt.Println(b.Appcenter(ask_singlekey(listname[32])))
 	} else if choose == 34 {
-		fmt.Println(b.Youtube(ask_singlekey(arr[33])))
+		fmt.Println(b.Youtube(ask_singlekey(listname[33])))
 	} else if choose == 35 {
-		fmt.Println(b.Wakatime(ask_singlekey(arr[34])))
+		fmt.Println(b.Wakatime(ask_singlekey(listname[34])))
 	} else if choose == 36 {
-		q1, q2 := ask_secondkey(arr[35], "WPEngine Account", "WPEngine Key")
+		q1, q2 := ask_secondkey(listname[35], "WPEngine Account", "WPEngine Key")
 		fmt.Println(b.WPEngine(q1, q2))
 	} else if choose == 37 {
-		q1, q2 := ask_secondkey(arr[36], "Zendesk Subdomain", "Zendesk Token")
+		q1, q2 := ask_secondkey(listname[36], "Zendesk Subdomain", "Zendesk Token")
 		fmt.Println(b.Zendesk(q1, q2))
 	}
 }
